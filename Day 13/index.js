5f984720d83ca5167800c4e809ec82c0637f3f48
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2024-07-25 11:40:53 +0530

    Day 13 Modules

---
+++ Day 13/index.js
@@ -0,0 +1,5 @@
+const {add,obj}=require('./module')
+// const obj=require('./module')
+
+console.log(add(2,3));
+console.log(obj);
---
+++ Day 13/module.js
@@ -0,0 +1,11 @@
+const add= function(n,m){
+    return n+m
+}
+
+const obj={
+    name:"Prakhar",
+    age:22
+}
+module.exports={add , obj}
+
+
